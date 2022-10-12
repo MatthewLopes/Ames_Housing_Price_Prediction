168b146375005ce87b4216ba6b5c1743b6321534
Spencer Buja <spencerb@ziprecruiter.com>
2022-10-11 17:16:37 -0700

    fixed test.y issue

--- driver_script.R
+++ driver_script.R
@@ -1,11 +1,16 @@
+file_1 = "jkovach2_mymain_9pm.R"
+file_2 = "accuracy_evaluation.R"
+
 for (j in 1:10) {
   
-  directory = paste0("test_runs/test_run", j)
+ directory = paste0("test_runs/test_run", j)
+ file.copy(file_1, directory)
+ file.copy(file_2, directory)
+ 
+ setwd(directory)
+ source(file_1)
+ source(file_2)
   
-  setwd(directory)
-  
-  source("jkovach2_mymain_9pm.R")
-  source("accuracy_evaluation.R")
   
   setwd("../..")
 }
